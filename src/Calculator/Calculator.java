60fb560f723969fa5f56dbcba4bd64a0037b8ecd
jasondai1991 <jasondai@jasondaidembp.gateway.sonic.net>
2018-04-12 20:47:02 -0700

    skeleton

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,13 @@
+package Calculator;
+
+public class Calculator {
+    private String name;
+
+    public Calculator(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
